dd6369d8eafcc49cfba844acf2c59be48e1978d4
Brielle McCormick <briellemccormick@Brielles-MBP.fios-router.home>
2020-10-07 13:15:54 -0400

    added navbar and jumbotron

---
+++ client/src/components/Jumbotron/index.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+
+function Jumbotron() {
+    return (
+        <div className="jumbotron col-10 mx-auto mt-5">
+            <h1 className="display-4">Hello, welcome to Google Book Search!</h1>
+            <p className="lead">This react app allows you to search and save books of interest.</p>
+        </div>
+    );
+}
+
+export default Jumbotron;
